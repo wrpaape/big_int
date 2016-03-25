d338a5c8f600a300188d5dd1e234e604dffdd335
Reid Paape <wrpaape@gmail.com>
2016-03-25 17:51:03 -0500

    another draft of add

--- src/big_int/add.h
+++ src/big_int/add.h
@@ -3,7 +3,6 @@
 /* EXTERNAL DEPENDENCIES ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
 
 #include "big_int/subtract.h"
-#include "big_int/multiply.h"
 
 /* EXTERNAL DEPENDENCIES ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲ */
 
@@ -24,7 +23,7 @@ void add_big_ints_same_sign(struct BigInt *result,
 
 
 
-/* EXTERNAL API ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+/* EXTERNAL INLINE FUNCTION DEFINITIONS  ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
 
 inline void add_big_ints(struct BigInt *result,
 			 struct BigInt *big1,
@@ -35,39 +34,5 @@ inline void add_big_ints(struct BigInt *result,
 	else
 		subtract_big_ints_same_sign(result, big1, big2);
 }
-
-/* { */
-
-/* 	/1* compare magnitudes before calling implementation */
-/* 	 * ================================================================== *1/ */
-/* 	switch (compare_big_int_mags(big1, big2)) { */
-
-/* 	case POS: */
-/* 		/1* big1 > big2, call low level add function */
-/* 		 * ========================================================== *1/ */
-/* 		add_big_ints_same_sign_ordered(result, big1, big2); */
-/* 		return; */
-
-/* 	case NEG: */
-/* 		/1* big1 < big1, correct param ordering before calling add */
-/* 		 * ========================================================== *1/ */
-/* 		add_big_ints_same_sign_ordered(result, big2, big1); */
-/* 		return; */
-
-
-/* 	default: */
-/* 		/1* abs(big1) == abs(big2) */
-/* 		 * */
-/* 		 *  → big1 + big2 */
-/* 		 *  = 2big1 */
-/* 		 *  = arithmetic shift left by 1 on big1 */
-/* 		 * */
-/* 		 *  delegate addition to more efficient bitwise operation */
-/* 		 * ========================================================== *1/ */
-/* 		big_int_ash_left(result, big1, 1lu); */
-/* 		return; */
-/* 	} */
-/* } */
-
-/* EXTERNAL API ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲ */
+/* EXTERNAL INLINE FUNCITON DEFINITIONS ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲ */
 #endif /* ifndef BIG_INT_ADD_H_ */
