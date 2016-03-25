6aa91e28336ef5241bb26013bc693dc816c3c814
Reid Paape <wrpaape@gmail.com>
2016-03-25 13:47:23 -0500

    still fleshing out api functions of add

--- src/big_int/add.h
+++ src/big_int/add.h
@@ -2,8 +2,9 @@
 #define BIG_INT_ADD_H_
 /* EXTERNAL DEPENDENCIES ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
 
-#include "big_int/multiply.h"
+#include "big_int/utils.h"
 #include "big_int/subtract.h"
+#include "big_int/multiply.h"
 
 /* EXTERNAL DEPENDENCIES ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲ */
 
@@ -11,9 +12,9 @@
 
 /* TOP-LEVEL FUNCTION PROTOTYPES ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
 
-void add_big_ints(struct BigInt *result,
-		  struct BigInt *big1,
-		  struct BigInt *big2);
+void add_big_ints_same_sign_ordered(struct BigInt *result,
+				    struct BigInt *big1,
+				    struct BigInt *big2);
 
 /* TOP-LEVEL FUNCTION PROTOTYPES ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲ */
 
@@ -30,35 +31,48 @@ inline void add_big_ints(struct BigInt *result,
 			 struct BigInt *big1,
 			 struct BigInt *big2)
 {
-	/* ensure big1->word_count >= big2->word_count
-	 * ================================================================== */
+	if (big1->sign == big2->sign)
+		add_big_ints_same_sign(result, big1, big2);
+	else
+		subtract_big_ints(result, big1, big2);
 
-	if (big2->word_count > big1->word_count) {
-		struct
-	}
-	switch (big1->sign) {
-	case POS:
-
-		switch (/* variable */) {
-		case POS:
-			return add_big_ints_same_sign(result, big1, big2);
-
-		case NEG:
-		default:
-		}
-
-	case NEG:
-	default:
-	}
-
-	/* ensure big1->word_count >= big2->word_count
-	 * ================================================================== */
+	return;
 }
 
 inline void add_big_ints_same_sign(struct BigInt *result,
 				   struct BigInt *big1,
 				   struct BigInt *big2)
 {
+
+	/* compare magnitudes before calling implementation
+	 * ================================================================== */
+	switch (compare_big_int_mags(big1, big2)) {
+
+	case POS:
+		/* big1 > big2, call low level add function
+		 * ========================================================== */
+		add_big_ints_same_sign_ordered(result, big1, big2);
+		return;
+
+	case NEG:
+		/* big1 < big1, correct param ordering before calling add
+		 * ========================================================== */
+		add_big_ints_same_sign_ordered(result, big2, big1);
+		return;
+
+
+	default:
+		/* abs(big1) == abs(big2)
+		 *
+		 *  → big1 + big2
+		 *  = 2big1
+		 *  = arithmetic shift left by 1 on big1
+		 *
+		 *  delegate addition to more efficient bitwise operation
+		 * ========================================================== */
+		big_int_ash_left(result, big1, 1lu);
+		return;
+	}
 }
 
 /* EXTERNAL API ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲ */
---
+++ src/big_int/subtract.c
@@ -0,0 +1,35 @@
+/* EXTERNAL DEPENDENCIES ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+
+#include "big_int/globals.h"
+#include "big_int/memory.h"
+#include "big_int/add.h"
+#include "big_int/subtract.h"
+
+/* EXTERNAL DEPENDENCIES ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲ */
+
+
+/* CONSTANTS ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+/* CONSTANTS ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲ */
+
+
+
+/* FUNCTION-LIKE MACROS ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+/* FUNCTION-LIKE MACROS ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲ */
+
+
+
+/* TOP-LEVEL FUNCTION DEFINITIONS ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+
+void subtract_big_ints(struct BigInt *result,
+		      struct BigInt *big,
+		      const size_t shift)
+{
+}
+
+
+/* TOP-LEVEL FUNCTION DEFINITIONS ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲ */
+
+
+
+/* HELPER FUNCTION DEFINITIONS ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ */
+/* HELPER FUNCTION DEFINITIONS ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲ */
