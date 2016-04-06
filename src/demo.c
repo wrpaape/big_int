b205fd3cc903d01b9d5dde31b22d26070d8f9d9a
Reid Paape <wrpaape@gmail.com>
2016-04-06 09:14:25 -0500

    compiling again, working on adding same sign ints

--- src/demo.c
+++ src/demo.c
@@ -8,7 +8,7 @@ int main(void)
 	struct BigInt *big2 = init_big_int(INT_MAX);
 	struct BigInt *big3 = init_big_int(0);
 
-	add_big_ints(big1, big2, big3);
+	add_big_ints(big3, big1, big2);
 
 
 	printf("big1: %s\n", big_int_to_string(big1));
